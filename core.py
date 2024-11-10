764c3a3ffd96f31d193d0bea359808872a91f7ba
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-10 11:50:06 +0300

    INSERT…RETURNING

--- core.py
+++ core.py
@@ -5,29 +5,7 @@ from database import get_db_engine
 
 engine = get_db_engine()
 
-scalar_subq = (
-    select(user_table.c.id)
-    .where(user_table.c.name is bindparam("username"))
-    .scalar_subquery()
+insert_stmt = insert(address_table).returning(
+    address_table.c.id, address_table.c.email_address
 )
-
-with engine.connect() as conn:
-    result = conn.execute(
-        insert(address_table).values(user_id=scalar_subq),
-        [
-            {
-                "username": "spongebob",
-                "email_address": "spongebob@sqlalchemy.org",
-            },
-            {
-                "username": "sandy",
-                "email_address": "sandy@sqlalchemy.org"
-            },
-            {
-                "username": "sandy",
-                "email_address": "sandy@squirrelpower.org"},
-        ],
-    )
-    # conn.commit()
-
-# print(insert(user_table).values().compile(engine)) - insert default values
+print(insert_stmt)
