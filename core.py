0e5c7c394ee688cb50d667c847d0e1d03140c334
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-10 11:54:24 +0300

    INSERT…FROM SELECT

--- core.py
+++ core.py
@@ -9,3 +9,17 @@ insert_stmt = insert(address_table).returning(
     address_table.c.id, address_table.c.email_address
 )
 print(insert_stmt)
+
+# Его также можно комбинировать с Insert.from_select(),
+# как в примере ниже, который основан на примере, указанном в INSERT…FROM SELECT
+#                                |
+#                                V
+# ------------------------------------------------------------------------
+# Эта конструкция используется, когда требуется скопировать данные из какой-то другой части базы данных
+# непосредственно в новый набор строк, без фактического извлечения и повторной отправки данных от клиента.
+print('-----------------------------------------------------------------------')
+select_stmt = select(user_table.c.id, user_table.c.name + "@aol.com")
+insert_stmt = insert(address_table).from_select(
+    ["user_id", "email_address"], select_stmt
+)
+print(insert_stmt)
