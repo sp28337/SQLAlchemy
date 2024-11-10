be43a29e323501360bf409555b8751417f24328a
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-10 09:21:08 +0300

    Executing the Statement

--- core.py
+++ core.py
@@ -3,7 +3,14 @@ from sqlalchemy import insert
 from core_models import *
 from database import engine
 
+
 stmt = insert(user_table).values(name="spongebob", fullname="Spongebob Squarepants")
 print(f'{'\n [!]  '}{stmt}{'\n'}')
 compiled = stmt.compile()
 print(f'{'\n [!]  '}{compiled.params}{'\n'}')
+
+
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    print(f'{'\n [!]  '}{result}{'\n'}')
+    conn.commit()
