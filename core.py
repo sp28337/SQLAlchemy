2132cd90dc5212b49ea9bf88e96cdf6d72ad0cc7
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-10 12:44:19 +0300

    Setting the COLUMNS and FROM clause

--- core.py
+++ core.py
@@ -1,31 +1,23 @@
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from core_models import *
 from database import get_db_engine
-from orm_models import User
 
 engine = get_db_engine()
 
-stmt = select(user_table).where(user_table.c.name == "spongebob")
 
-print(stmt)
+print(select(user_table))
 print()
+# ------------------------------------------------------------------------------------
+# Чтобы выполнить `SELECT` из отдельных столбцов с использованием подхода `Core`,
+# `Column` доступ к объектам осуществляется из `Table.c` метода доступа,
+# и их можно отправлять напрямую; предложение `FROM` будет выведено как набор всех `Table`
+# и других `FromClause` объектов, представленных этими столбцами:
 
-with engine.connect() as conn:
-    for row in conn.execute(stmt):
-        print()
-        print(row)
-        print()
-# ---------------------------------------------------------------------------------------------------------------------
-# При использовании ORM, особенно с select()конструкцией, составленной на основе сущностей ORM,
-# нам нужно будет выполнить ее с помощью Session.execute()метода в Session; используя этот подход,
-# мы продолжаем получать Row объекты из результата, однако эти строки теперь могут включать в себя полные сущности,
-# такие как экземпляры класса User, как отдельные элементы в каждой строке:
+print(select(user_table.c.name, user_table.c.fullname))
+print()
+# -------------------------------------------------------------------------------------
+# В качестве альтернативы при использовании `FromClause.c` коллекции `any`, `FromClause`
+# например `Table`, можно указать несколько столбцов для a `select()` с помощью списка имен строк:
 
-stmt = select(User).where(User.name == "spongebob")
-with Session(engine) as session:
-    for row in session.execute(stmt):
-        print()
-        print(row)
-        print()
+print(select(user_table.c["name", "fullname"]))
