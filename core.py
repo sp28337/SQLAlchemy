471e39424ce676a6ffa2b6f2e8a2cb75353992d1
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 14:25:54 +0300

    The delete() SQL Expression Construct

--- core.py
+++ core.py
@@ -1,37 +1,13 @@
-from sqlalchemy import update, select
-from sqlalchemy.dialects import mysql
+from sqlalchemy import delete
 
-from core_models import user_table, address_table
-from database import get_db_engine
+from core_models import user_table
 
-engine = get_db_engine()
+# Функция delete() генерирует новый экземпляр Delete, представляющий оператор DELETE в SQL,
+# который удаляет строки из таблицы.
+#
+# С точки зрения API оператор delete() очень похож на конструкцию update(), традиционно не возвращающую строк,
+# но допускающую вариант RETURNING на некоторых бэкэндах баз данных.
 
+stmt = delete(user_table).where(user_table.c.name == "patrick")
+print(stmt)
 
-# Некоторые базы данных, такие как PostgreSQL и MySQL, поддерживают синтаксис «UPDATE FROM», где дополнительные
-# таблицы могут быть указаны непосредственно в специальном предложении FROM. Этот синтаксис будет сгенерирован неявно,
-# когда дополнительные таблицы находятся в предложении WHERE оператора:
-
-update_stmt = (
-    update(user_table)
-    .where(user_table.c.id == address_table.c.user_id)
-    .where(address_table.c.email_address == "patrick@aol.com")
-    .values(fullname="Pat")
-)
-print(update_stmt)
-
-print("-" * 60)
-# Также есть синтаксис MySQL, который может ОБНОВЛЯТЬ несколько таблиц. Для этого требуется, чтобы мы ссылались
-# на Table объекты в предложении VALUES, чтобы ссылаться на дополнительные таблицы:
-
-update_stmt = (
-    update(user_table)
-    .where(user_table.c.id == address_table.c.user_id)
-    .where(address_table.c.email_address == "patrick@aol.com")
-    .values(
-        {
-            user_table.c.fullname: "Pat",
-            address_table.c.email_address: "pat@aol.com",
-        }
-    )
-)
-print(update_stmt.compile(dialect=mysql.dialect()))
