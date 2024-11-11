f1c8ec6038f5d9706d8116e11fed194eb1d00f6c
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 14:18:09 +0300

    - UPDATE..FROM

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 from sqlalchemy import update, select
+from sqlalchemy.dialects import mysql
 
 from core_models import user_table, address_table
 from database import get_db_engine
@@ -6,19 +7,31 @@ from database import get_db_engine
 engine = get_db_engine()
 
 
-# Оператор UPDATE может использовать строки в других таблицах с помощью коррелированного подзапроса.
-# Подзапрос может использоваться в любом месте, где может быть размещено выражение столбца:
+# Некоторые базы данных, такие как PostgreSQL и MySQL, поддерживают синтаксис «UPDATE FROM», где дополнительные
+# таблицы могут быть указаны непосредственно в специальном предложении FROM. Этот синтаксис будет сгенерирован неявно,
+# когда дополнительные таблицы находятся в предложении WHERE оператора:
 
-scalar_subq = (
-    select(address_table.c.email_address)
-    .where(address_table.c.user_id == user_table.c.id)
-    .order_by(address_table.c.id)
-    .limit(1)
-    .scalar_subquery()
+update_stmt = (
+    update(user_table)
+    .where(user_table.c.id == address_table.c.user_id)
+    .where(address_table.c.email_address == "patrick@aol.com")
+    .values(fullname="Pat")
 )
-print(scalar_subq)
+print(update_stmt)
 
 print("-" * 60)
+# Также есть синтаксис MySQL, который может ОБНОВЛЯТЬ несколько таблиц. Для этого требуется, чтобы мы ссылались
+# на Table объекты в предложении VALUES, чтобы ссылаться на дополнительные таблицы:
 
-update_stmt = update(user_table).values(fullname=scalar_subq)
-print(update_stmt)
+update_stmt = (
+    update(user_table)
+    .where(user_table.c.id == address_table.c.user_id)
+    .where(address_table.c.email_address == "patrick@aol.com")
+    .values(
+        {
+            user_table.c.fullname: "Pat",
+            address_table.c.email_address: "pat@aol.com",
+        }
+    )
+)
+print(update_stmt.compile(dialect=mysql.dialect()))
