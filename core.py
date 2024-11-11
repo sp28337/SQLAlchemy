d78f14b509a3571dde7e4c408287fc26c01607d2
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 11:16:24 +0300

    ORDER BY, GROUP BY, HAVING

--- core.py
+++ core.py
@@ -1,19 +1,6 @@
-from sqlalchemy import select
-
-from core_models import *
-from database import get_db_engine
-
-engine = get_db_engine()
-
-
-# Оба метода Select.join() и Select.join_from() принимают аргументы-ключевые слова Select.join.isouter,
-# Select.join.full которые отобразят LEFT OUTER JOIN и FULL OUTER JOIN соответственно:
-
-print(select(user_table).join(address_table, isouter=True))
-print("-" * 60)
-print(select(user_table).join(address_table, full=True))
-
-# Существует также метод Select.outerjoin(), эквивалентный использованию ..join(..., isouter=True)
-
-# В SQL также есть «RIGHT OUTER JOIN». SQLAlchemy не отображает его напрямую;
-# вместо этого измените порядок таблиц на обратный и используйте «LEFT OUTER JOIN».
+# Оператор SELECT включает предложение ORDER BY, которое используется для возврата выбранных строк в заданном порядке.
+#
+# Предложение GROUP BY построено аналогично предложению ORDER BY и имеет целью подразделение выбранных строк на
+# определенные группы, на которых могут быть вызваны агрегатные функции. Предложение HAVING обычно используется
+# с GROUP BY и имеет форму, похожую на предложение WHERE, за исключением того, что оно применяется к агрегированным
+# функциям, используемым внутри групп.
