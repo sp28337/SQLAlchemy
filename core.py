89508a3a9203309ce042b3e577c11424c12a23bb
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 10:35:56 +0300

    Explicit FROM clauses and JOINs

--- core.py
+++ core.py
@@ -1,62 +1,45 @@
-from sqlalchemy import select, and_, or_
+from sqlalchemy import select, func
 
 from core_models import *
 from database import get_db_engine
-from orm_models import Address, User
 
 engine = get_db_engine()
 
 
-# SQLAlchemy позволяет нам составлять выражения SQL, такие как name = 'squidward' или user_id > 10,
-# используя стандартные операторы Python в сочетании с Column и подобными объектами.
-# Для булевых выражений большинство операторов Python, таких как ==, >=  >, и т. д.,
-# генерируют новые объекты SQL Expression, а не простые булевы значения True/False:
-
-print(user_table.c.name == "squidward")
-print(address_table.c.user_id > 10)
+# Если мы выбираем один столбец из конкретной таблицы, то Table указывается после FROM:
+print(select(user_table.c.name))
 
 print("-" * 60)
-# Мы можем использовать подобные выражения для генерации предложения WHERE,
-# передавая полученные объекты методу Select.where():
+# Если бы мы выбирали столбцы из двух таблиц, то получили бы предложение FROM, разделенное запятыми:
 
-print(select(user_table).where(user_table.c.name == "squidward"))
+print(select(user_table.c.name, address_table.c.email_address))
 
-print("-" * 60)
-# Для создания нескольких выражений, объединенных с помощью AND,
-# Select.where() метод можно вызывать любое количество раз:
+print("." * 60)
+# Для того, чтобы ОБЪЕДИНИТЬ эти две таблицы вместе, мы обычно используем один из двух методов Select.
+# Первый — это Select.join_from() метод, который позволяет нам явно указать левую и правую сторону ОБЪЕДИНЕНИЯ:
 
 print(
-    select(address_table.c.email_address)
-    .where(user_table.c.name == "squidward")
-    .where(address_table.c.user_id == user_table.c.id)
-)
-
-print("-" * 60)
-# Один вызов Select.where() также принимает несколько выражений с тем же эффектом:
-
-print(
-    select(address_table.c.email_address).where(
-        user_table.c.name == "squidward",
-        address_table.c.user_id == user_table.c.id,
+    select(user_table.c.name, address_table.c.email_address).join_from(
+        user_table, address_table
     )
 )
 
-print("-" * 60)
-# Союзы «И» и «ИЛИ» доступны напрямую с использованием функций and_()и or_(),
-# как показано ниже в ORM:
+print("_" * 60)
+# Другой метод Select.join() указывает только правую сторону JOIN, левая сторона выводится:
 
-print(
-    select(Address.email_address).where(
-        and_(
-            or_(User.name == "squidward", User.name == "sandy"),
-            Address.user_id == User.id,
-        )
-    )
-)
+print(select(user_table.c.name, address_table.c.email_address).join(address_table))
 
-print("-" * 60)
-# Для простых сравнений «равенства» с одной сущностью также существует популярный метод, известный как
-# Select.filter_by(), который принимает ключевые аргументы, соответствующие ключам столбцов или именам атрибутов ORM.
-# Он будет фильтровать по самому левому выражению FROM или последней присоединенной сущности:
+print("*" * 60)
+# У нас также есть возможность явно добавлять элементы в предложение FROM, если это не выводится так, как мы хотим,
+# из предложения columns. Мы используем Select.select_from() метод, чтобы добиться того, как показано ниже,
+# где мы устанавливаем user_table в качестве первого элемента в предложении FROM и Select.join() устанавливаем
+# address_table в качестве второго:
 
-print(select(User).filter_by(name="spongebob", fullname="Spongebob Squarepants"))
+print(select(address_table.c.email_address).select_from(user_table).join(address_table))
+
+print("`" * 60)
+# Другой пример, где мы могли бы захотеть использовать Select.select_from(), если в нашем предложении columns
+# недостаточно информации для предоставления предложения FROM. Например, чтобы SELECT из общего выражения SQL count(*),
+# мы используем элемент SQLAlchemy, известный как sqlalchemy.sql.expression.func для создания функции SQL count():
+
+print(select(func.count("*")).select_from(user_table))
