139168cfdf666c5a2e26cc435e40b0e4c8745a3d
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 13:07:33 +0300

    - Selecting ORM Entities from Unions

--- core.py
+++ core.py
@@ -1,40 +1,45 @@
 from sqlalchemy import select, union_all
+from sqlalchemy.orm import aliased
+from database import get_db_session
+from orm_models import User
 
-from core_models import address_table, user_table
-from database import get_db_engine
+# Предыдущие примеры иллюстрировали, как построить UNION для двух Tableо бъектов, чтобы затем
+# вернуть строки базы данных. Если мы хотим использовать UNION или другую операцию над множествами для выбора строк,
+# которые затем получаем как объекты ORM, есть два подхода, которые можно использовать.
+# В обоих случаях мы сначала создаем объект select() or CompoundSelect, который представляет
+# оператор SELECT / UNION / etc, который мы хотим выполнить; этот оператор должен быть составлен против
+# целевых сущностей ORM или их базовых сопоставленных Tableо бъектов:
 
-engine = get_db_engine()
-
-# В SQL операторы SELECT могут быть объединены вместе с помощью операции UNION или UNION ALL SQL,
-# которая создает набор всех строк, созданных одним или несколькими операторами вместе.
-# Также возможны другие операции над множествами, такие как INTERSECT [ALL] и EXCEPT [ALL].
-#
-# Конструкция SQLAlchemy Select поддерживает композиции такого рода с использованием таких функций,
-# как union(), intersect() и except_(), а также «всех» аналогов union_all(), intersect_all() и except_all().
-# Все эти функции принимают произвольное количество подвыбираемых элементов, которые обычно являются
-# Select конструкциями, но также могут быть существующей композицией.
-#
-# Конструкция, созданная этими функциями, — это CompoundSelect, которая используется таким же образом,
-# как и Select конструкция, за исключением того, что у нее меньше методов. CompoundSelect Созданная,
-# union_all() например, может быть вызвана напрямую с помощью Connection.execute():
-
-stmt1 = select(user_table).where(user_table.c.name == "sandy")
-stmt2 = select(user_table).where(user_table.c.name == "spongebob")
+stmt1 = select(User).where(User.name == "sandy")
+stmt2 = select(User).where(User.name == "spongebob")
 u = union_all(stmt1, stmt2)
-with engine.connect() as conn:
-    result = conn.execute(u)
-    print(result.all())
 
-print("---" * 20)
-# Чтобы использовать CompoundSelect в качестве подзапроса, как и в Select случае с SelectBase.subquery() методом,
-# который создаст Subquery объект с FromClause.c коллекцией, на которую можно ссылаться во вложенном select():
+print(stmt1, stmt2, u, sep='\n\n')
 
-u_subq = u.subquery()
-stmt = (
-    select(u_subq.c.name, address_table.c.email_address)
-    .join_from(address_table, u_subq)
-    .order_by(u_subq.c.name, address_table.c.email_address)
-)
-with engine.connect() as conn:
-    result = conn.execute(stmt)
-    print(result.all())
+print("-" * 60)
+# Для простого SELECT с UNION, который еще не вложен в подзапрос, их часто можно использовать в контексте извлечения
+# объектов ORM с помощью Select.from_statement() метода. При таком подходе оператор UNION представляет весь запрос;
+# после Select.from_statement()использования не может быть добавлено никаких дополнительных критериев:
+
+orm_stmt = select(User).from_statement(u)
+
+print("-" * 60)
+with get_db_session() as session:
+    for obj in session.execute(orm_stmt).scalars():
+        print(obj)
+
+print("_" * 60)
+# Чтобы использовать UNION или другую конструкцию, связанную с множеством, в качестве компонента,
+# связанного с сущностью, более гибко, CompoundSelect конструкция может быть организована в подзапрос
+# с использованием CompoundSelect.subquery(), который затем связывается с объектами ORM с помощью
+# aliased() функции. Это работает так же, как представлено в ORM Entity Subqueries/CTE, чтобы
+# сначала создать ad hoc «отображение» нашей желаемой сущности в подзапрос, а затем выбрать из этой новой сущности,
+# как если бы это был любой другой отображенный класс. В примере ниже мы можем добавить дополнительные критерии,
+# такие как ORDER BY, за пределами самого UNION, поскольку мы можем фильтровать или упорядочивать по столбцам,
+# экспортированным подзапросом:
+
+user_alias = aliased(User, u.subquery())
+orm_stmt = select(user_alias).order_by(user_alias.id)
+with get_db_session() as session:
+    for obj in session.execute(orm_stmt).scalars():
+        print(obj)
