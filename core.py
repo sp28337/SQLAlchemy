cf06ffa45c0466a3d80eb929ef1fadbea1074aae
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 13:53:07 +0300

    - Data Casts and Type Coercion

--- core.py
+++ core.py
@@ -1,44 +1,26 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, cast, String, JSON
 
-from core_models import address_table, user_table
+from core_models import user_table
 from database import get_db_engine
 
 engine = get_db_engine()
 
-# Распространенной функцией, используемой с оконными функциями, является row_number() функция,
-# которая просто подсчитывает строки. Мы можем разделить это количество строк на имя пользователя,
-# чтобы подсчитать адреса электронной почты отдельных пользователей:
+# В SQL нам часто требуется явно указать тип данных выражения, либо чтобы сообщить базе данных,
+# какой тип ожидается в иначе неоднозначном выражении, либо в некоторых случаях, когда мы хотим преобразовать
+# подразумеваемый тип данных выражения SQL во что-то другое. Для этой задачи используется ключевое слово SQL CAST,
+# которое в SQLAlchemy предоставляется функцией cast(). Эта функция принимает выражение столбца и объект типа
+# данных в качестве аргументов, как показано ниже, где мы создаем выражение SQL из объекта столбца
+# :CAST(user_account.id AS VARCHAR)user_table.c.id
 
-stmt = (
-    select(
-        func.row_number().over(partition_by=user_table.c.name),
-        user_table.c.name,
-        address_table.c.email_address,
-    )
-    .select_from(user_table)
-    .join(address_table)
-)
-print(stmt)
-print("-" * 60)
-
+stmt = select(cast(user_table.c.id, String))
 with engine.connect() as conn:
     result = conn.execute(stmt)
     print(result.all())
 
-# Выше FunctionElement.over.partition_by параметр используется для того, чтобы предложение было отображено внутри
-# предложения OVER. Мы также можем использовать предложение,
-# используя :PARTITION BY ORDER BY FunctionElement.over.order_by
 print("-" * 60)
+# Функция cast() не только отображает синтаксис SQL CAST, она также создает выражение столбца SQLAlchemy,
+# которое будет действовать как заданный тип данных на стороне Python. Строковое выражение, которое должно cast()
+# получить JSON индекс JSON и операторы сравнения, например:
 
-stmt = (
-    select(
-        func.count().over(order_by=user_table.c.name),
-        user_table.c.name,
-        address_table.c.email_address,
-    )
-    .select_from(user_table)
-    .join(address_table)
-)
-with engine.connect() as conn:
-    result = conn.execute(stmt)
-    print(result.all())
+print(cast("{'a': 'b'}", JSON)["a"])
+
