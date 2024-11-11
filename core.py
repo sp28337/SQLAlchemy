07ebc0dfc57aa5d5e4164084ef6293fafc33e040
Pavel Tarakanov <pavel191094@icloud.com>
2024-11-11 14:29:26 +0300

    - Multiple Table Deletes

--- core.py
+++ core.py
@@ -1,13 +1,15 @@
 from sqlalchemy import delete
+from sqlalchemy.dialects import mysql
 
-from core_models import user_table
+from core_models import user_table, address_table
 
-# Функция delete() генерирует новый экземпляр Delete, представляющий оператор DELETE в SQL,
-# который удаляет строки из таблицы.
-#
-# С точки зрения API оператор delete() очень похож на конструкцию update(), традиционно не возвращающую строк,
-# но допускающую вариант RETURNING на некоторых бэкэндах баз данных.
+# Как и Update, Delete поддерживает использование коррелированных подзапросов в предложении WHERE,
+# а также специфичные для бэкэнда многотабличные синтаксисы, например, в MySQL:DELETE FROM..USING
 
-stmt = delete(user_table).where(user_table.c.name == "patrick")
-print(stmt)
+delete_stmt = (
+    delete(user_table)
+    .where(user_table.c.id == address_table.c.user_id)
+    .where(address_table.c.email_address == "patrick@aol.com")
+)
 
+print(delete_stmt.compile(dialect=mysql.dialect()))
